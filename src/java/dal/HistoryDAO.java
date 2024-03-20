3b6d6eb8b45637039c612075aaeae7a138179daa
1cuhp <hoangphucagg@gmail.com>
2024-03-20 20:21:54 +0700

    updateUI History

--- src/java/dal/HistoryDAO.java
+++ src/java/dal/HistoryDAO.java
@@ -24,7 +24,14 @@ public class HistoryDAO extends DBContext{
             stm.setString(1, userid);
             ResultSet rs = stm.executeQuery();
             while(rs.next()){
-                account.add(new Booking(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8)));
+                account.add(new Booking(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getString(3), 
+                        rs.getString(4), 
+                        rs.getString(5), 
+                        rs.getString(6), 
+                        rs.getString(7), 
+                        rs.getString(8)));
             }
         }catch(SQLException e){
             e.printStackTrace();
